41407e22ae92fa69d144a8c14c73d26f94fcaec6
Jake Jenne <jake.jenne@nuvi.com>
2019-08-06 08:38:58 -0600

    bing by cit

--- bing-by-city.js
+++ bing-by-city.js
@@ -5,6 +5,7 @@ const rp = require('request-promise');
 const Entities = require('html-entities').XmlEntities;
 const entities = new Entities();
 
+const searchDomain = 'https://www.bing.com';
 const searchPrefix = '+"TSHQ License Agreement" +email';
 const urlPrefix = 'https://www.bing.com/search?qs=n&form=QBLH&sp=-1&pq=%22tshq+license+agreement%22+email&sc=1-30&sk=&cvid=D8E807C40FEE40DE9AC2ED8FF14804F7&q=';
 
@@ -17,7 +18,7 @@ if (!OUT_FILE) {
 }
 
 
-console.log('crawling...');
+console.log('----------- begin ------------');
 
 return fs.readFile(OUT_FILE, 'utf8')
 .then((data) => {
@@ -29,7 +30,7 @@ return fs.readFile(OUT_FILE, 'utf8')
   .then((csv) => {
     const cities = csv.split('\n').slice(1).map((line) => {
       const fields = line.split(',');
-      const city = trim(fields[8]);
+      const city = parseCity(fields[8]);
       const state = trim(fields[9]);
       const population = parseInt(trim(fields[10]));
       return {
@@ -37,7 +38,7 @@ return fs.readFile(OUT_FILE, 'utf8')
         state,
         population,
       }
-    }).filter(entry => entry.city !== entry.state && entry.population >= 15000);
+    }).filter(entry => entry.city !== entry.state && entry.population >= 10000 && entry.city && entry.state && !entry.city.toLowerCase().includes('balance of'));
 
     const iter = cities.entries();
     return iterateCities(list, iter);
@@ -61,12 +62,17 @@ function iterateCities(list, iter) {
 
   const record = iteration.value[1];
 
+  console.log(`city: '${record.city}'`);
+
   // If we've already done this city, skip it
   if (list.find(item => item.city === record.city && item.state === record.state)) {
     return iterateCities(list, iter);
   }
 
-  return crawl(record)
+  const searchTerm = encodeURIComponent(`${searchPrefix} +"${record.city}" +${record.state}`);
+  const crawlUrl = `${urlPrefix}+${searchTerm}`
+
+  return crawl(crawlUrl)
   .then((results) => {
     const cityResult = Object.assign({}, record);
     cityResult.leagues = results;
@@ -75,7 +81,7 @@ function iterateCities(list, iter) {
     const data = JSON.stringify(list);
     return fs.writeFile(OUT_FILE, data);
   })
-  .then(() => sleep(Math.random() * 10 + 10))
+  .then(() => sleep(Math.random() * 6 + 6))
   .then(() => iterateCities(list, iter));
 }
 
@@ -84,9 +90,8 @@ function iterateCities(list, iter) {
 
 
 
-function crawl (record) {
-  const searchTerm = encodeURIComponent(`${searchPrefix} +${record.city} +${record.state}`);
-  const crawlUrl = `${urlPrefix}+${searchTerm}`
+
+function crawl (crawlUrl, previousEmails = []) {
 
   console.log(`crawling ${crawlUrl}`);
 
@@ -110,15 +115,47 @@ function crawl (record) {
     console.log(links.join('\n'));
 
     // follow each link to a sports site
-    return Promise.all(links.map(l => followLink(l)));
-  })
-  .then((results) => {
-    return results.reduce((final, row) => {
-      if (_.isEmpty(row)) {
+    return Promise.all(links.map(l => followLink(l)))
+    .then((results) => {
+      return results.reduce((final, row) => {
+        if (_.isEmpty(row)) {
+          return final;
+        }
+        return final.concat(row);
+      }, []);
+    })
+    .then((emails) => {
+      // scan through the results and make sure there's something new or don't go on to next page
+      console.log(previousEmails, emails);
+      const hasOriginal = emails.reduce((final, item) => {
+        if (previousEmails.find(e => e.email === item.email)) {
+          return false;
+        }
         return final;
+      }, true);
+
+      if (!hasOriginal || _.isEmpty(emails)) {
+        console.log('no original []');
+        return Promise.resolve([]);
       }
-      return final.concat(row);
-    }, []);
+
+      const nextUrl = matchNextPageLink(html);
+      if (!nextUrl) {
+        console.log('no next page', emails);
+        return Promise.resolve(emails);
+      }
+
+      return sleep(Math.random() * 5 + 5)
+      .then(() => crawl(`${searchDomain}${entities.decode(nextUrl)}`, emails))
+      .then((next) => {
+        if (next) {
+          console.log('concatenate next', next);
+          emails = emails.concat(next);
+          console.log(emails);
+        }
+        return Promise.resolve(emails);
+      });
+    });
   });
 }
 
@@ -200,4 +237,29 @@ function trim(str) {
     return '';
   }
   return str.replace(/^\s+/, '').replace(/\s+$/, '');
+}
+
+function parseCity(str) {
+  if (!str) {
+    return null;
+  }
+  const r = / (city|town|township|borough|county)$/i;
+  return trim(trim(str).replace(r, ''));
+}
+
+function matchNextPageLink(html) {
+  const next_r = /<a[^>]+title="Next page" href="([^"]+)"/gmi;
+  const firstMatch = html.match(next_r);
+  if (firstMatch && firstMatch.length > 1) {
+    return firstMatch[1];
+  } else if (firstMatch && firstMatch.length === 1) {
+    const stupid_r = /href="([^"]+)"/i;
+    const match = firstMatch[0].match(stupid_r);
+    if (match && match.length > 1) {
+      return match[1];
+    }
+  }
+
+  console.error("  end of pages");
+  return null;
 }
